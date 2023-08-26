4c552f6bf0e6c2ffae29aaa01982d73f09cd4eb8
Tushar Rayamajhi <tusharrayamajhi956@gmail.com>
2023-08-26 18:13:58 +0545

    update

---
+++ interfaces.java
@@ -0,0 +1,6 @@
+
+public class interfaces {
+    public static void main(String[] args) {
+
+    }
+}
--- nonrepetingletter.java
+++ nonrepetingletter.java
@@ -28,17 +28,30 @@ public class nonrepetingletter {
             q.add(q.remove());
         }
     }
+    public static void reverse(Queue<Integer> q){
+        Stack<Integer> s = new Stack<>();
+        while(!q.isEmpty()){
+            s.push(q.remove());
+        }
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+    }
+    public static void generateBinaryNumbers(int N) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("1");
+
+        for (int i = 0; i < N; i++) {
+            String binary = queue.remove();
+            System.out.println(binary);
+
+            queue.add(binary + "0");
+            queue.add(binary + "1");
+        }
+    }
     public static void main(String[] args) {
         // String str = "aabccxb";
         // System.out.println(str.indexOf('b'));
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 1;i<=10;i++){
-            q.add(i);
-        }
-        System.out.println(q.size());
-        interleaftwohalfqueus(q);
-        while(!q.isEmpty()){
-            System.out.print(q.remove() + " ");
-        }
+        generateBinaryNumbers(10);
     }
 }
