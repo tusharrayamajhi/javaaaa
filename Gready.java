eb741cbb82eaa88aa8d3414bbcd7f38b3ed879ff
Tushar Rayamajhi <tusharrayamajhi956@gmail.com>
2023-08-30 14:02:21 +0545

    update

--- Gready.java
+++ Gready.java
@@ -1,31 +1,29 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 
 public class Gready{
-    public static void activitySelect(int start[], int end[]){
-        int activ[][] = new int[start.length][end.length];
-        for(int i=0;i<start.length;i++){
-            activ[i][0] = i;
-            activ[i][1] = start[i];
-            activ[i][2] = end[i];
+    public static void activitySelect(int start[],int end[]){
+        int arr[][] = new int[start.length][end.length];
+        for(int i = 0;i<start.length;i++){
+            arr[i][0] = i;
+            arr[i][1] = start[i];
+            arr[i][2] = end[i];
         }
-        Arrays.sort(activ,Comparator.comparingDouble(o->o[2]));
-        ArrayList<Integer> arr = new ArrayList<>();
-        int total = 1;
-        arr.add(activ[0][0]);
-        int last = activ[0][2];
-        for(int i = 0;i<end.length;i++){
-            if(activ[i][1] >= last){
-                total++;
-                last = activ[i][2]; 
-                arr.add(activ[i][0]);
+        Arrays.sort(arr,Comparator.comparingDouble(o->o[2]));
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(arr[0][0]);
+        int minh = arr[0][2];
+        for(int i = 0;i<start.length;i++){
+            if(arr[i][1]>=minh){
+                ans.add(arr[i][0]);
+                minh = arr[i][2];
             }
         }
-        for(int i = 0;i<arr.size();i++){
-            System.out.print("A" + arr.get(i)+" ");
+        for(int i = 0;i<ans.size();i++){
+            System.out.print("A"+ans.get(i)+" ");
         }
-        System.out.println();
     }
     public static void main(String args[]){
         int start[] = {1,0,3,5,8,5};
